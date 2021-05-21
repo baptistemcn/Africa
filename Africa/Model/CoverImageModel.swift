0a1a07945cebf1e0f0c1ea3f6d91bd59b18b7169
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-21 12:46:16 +0200

    feat: Images Slider w/ JSON Decoder

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Baptiste on 21/05/2021.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
