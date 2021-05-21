b7d45a94e42f9601c3889eeaecaf837df2d912a1
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-21 07:25:56 +0200

    init ✨

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Baptiste on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
