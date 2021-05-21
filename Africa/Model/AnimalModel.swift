9032bf708c3b145ca818d8b84f82c747ede01ba3
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-21 15:30:05 +0200

    feat: Animals List Item

--- Africa/Model/AnimalModel.swift
+++ Africa/Model/AnimalModel.swift
@@ -5,4 +5,15 @@
 //  Created by Baptiste on 21/05/2021.
 //
 
-import Foundation
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let  name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
