53723b6f13c307c1b3d3b6a39b5950258ed4ae23
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-23 18:46:59 +0200

    feat: Video List

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    // MARK: - PROPERTIES
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+        } //: NAVIGATION
     }
 }
 
+// MARK: - BODY
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
+            .previewDevice("iPhone 11")
     }
 }
---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,19 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Baptiste on 22/05/2021.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    
+    // Computed Property
+    var thumbanail: String {
+        "video-\(id)"
+    }
+}
