5b0d7c34d76bf5048f122f3c47da8cd471dd8b16
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-25 15:10:24 +0200

    refactor: GalleryView

---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,25 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Baptiste on 25/05/2021.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
